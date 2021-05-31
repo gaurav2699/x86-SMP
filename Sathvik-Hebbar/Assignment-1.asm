378557820863ccb3a562833bfcbe572486f4a857
sathuhebbar <sathu.hebbar@gmail.com>
2021-05-31 11:55:52 +0530

    Modified Assignment 1 to fix indentation

--- Sathvik-Hebbar/Assignment-1.asm
+++ Sathvik-Hebbar/Assignment-1.asm
@@ -6,27 +6,27 @@ section .data
 section .text
 _start:
     xor ecx, ecx
-	mov ebx, 1
-	mov edx, 1
-	cmp edx, [n]
-	je _special
-	inc edx
-	cmp edx, [n]
-	je safe_exit
-	inc edx
+    mov ebx, 1
+    mov edx, 1
+    cmp edx, [n]
+    je _special
+    inc edx
+    cmp edx, [n]
+    je safe_exit
+    inc edx
 
 _compute_fibonacci:
     mov eax, ebx
-	add ebx, ecx
-	mov ecx, eax
-	cmp edx, [n]
-	je safe_exit
-	inc edx
-	jmp _compute_fibonacci
+    add ebx, ecx
+    mov ecx, eax
+    cmp edx, [n]
+    je safe_exit
+    inc edx
+    jmp _compute_fibonacci
 
 _special:
     mov ebx, ecx
 
 safe_exit:
     mov eax, 1
-	int 0x80
+    int 0x80
